73c76ce81ffad5faf1f049ddf6d1dd8e68a256e0
LitanyOfBoredom <10302357+LitanyOfBoredom@users.noreply.github.com>
2020-04-04 22:05:30 -0500

    Added word puzzle

---
+++ Splash/Wordpuzzle.js
@@ -0,0 +1,40 @@
+function textChange()
+{
+    var text = document.getElementById("inputtext").value;
+    var output = document.getElementById("outputtext").innerText;
+    if(!output)
+        output = "aaaaaaaaaaa";
+
+    var newoutput = compute(text,output);
+
+    if(newoutput == compute("yqbuyqipivd", "trjjocikmij"))
+    {
+        document.getElementById("outputtext").innerText = newoutput + "    Placeholder for next puzzle";
+    }
+    else
+        document.getElementById("outputtext").innerText = newoutput;
+}
+
+function compute(text, output)
+{
+    var build = "";
+    for(var i = 0; i < text.length; i++)
+    {
+        var c = text[i];
+        if(c < 'a' || c > 'z')
+        {
+            build = build + output[i];
+            continue;
+        }
+        var x = c.charCodeAt(0);
+        var y = output.charCodeAt(i);
+        var z = x + y - 96;
+        if(z > 122)
+            z -= 26;
+        build = build + String.fromCharCode(z);
+    }
+    for(var i = text.length; i < 11; i++)
+        build = build + output[i];
+    
+    return build;
+}
